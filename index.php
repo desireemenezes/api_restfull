7ee8f07b91a5010cd26233653462c978a71ae84f
deemenezes@hotmail.com <deemenezes@hotmail.com>
2019-05-11 13:37:23 -0300

    add user

--- index.php
+++ index.php
@@ -5,20 +5,28 @@
 	/* ORGANIZANDO ARQUIVOS */
 	//include_once('ProdutoController.php');
 	include_once('Controller/ProdutoController.php');
+	include_once('UsuarioController.php');
 	include_once('DAO/ProdutoDAO.php');
+	include_once('DAO/UsuarioDAO.php');
 	include_once('Models/Produto.php');
+	include_once('Models/Usuario.php');
 
 	require './vendor/autoload.php';
 	$app = new \Slim\App;
 
 	//cria um grupo para setar na url os metodos
 	$app->group('/produtos', function() use ($app) {
+		
 		$app->get('','ProdutoController:listar');
 		$app->post('','ProdutoController:inserir');
-	
 		$app->get('/{id}','ProdutoController:buscarPorId');    
 		$app->put('/{id}','ProdutoController:atualizar');
 		$app->delete('/{id}', 'ProdutoController:deletar');
-	});
+	})->add('UsuarioController:validarToken');
+
+	$app->post('/usuarios','UsuarioController:inserir');
+	
+	$app->post('/auth','UsuarioController:autenticar');
 	
 	$app->run();
+	?>
